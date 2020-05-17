748b85a076c6728fcc980cf878a6c4f3bf4a6b56
Ismaïl <ismailben44@gmail.com>
2020-05-17 21:51:45 +0000

    add .gitignore & add logo with a blank background

--- js/load-content.js
+++ js/load-content.js
@@ -1,10 +1,12 @@
-function load_content( e ) {
-    var id = e.target.id;
-    if(id) {
-        document.getElementById("main").innerHTML='<object type="text/html" data="'+id+'.html" ></object>';
-    }
-    else {
-        console.log('nothing');
-    }
-     
+function loadContent(event) {
+  let id = event.target.id;
+  if (id) {
+    document.getElementById("main").innerHTML =
+      '<iframe src="' +
+      id +
+      '.html" width="100%" style="border-width: 0px";></iframe >';
+    // '<object type="text/html" data="' + id + '.html" ></object>';
+  } else {
+    console.log("nothing");
+  }
 }
