aa86a1c024f386c1c5c69d1f90b8f9d2741a71b3
Ismaïl <ismailben44@gmail.com>
2020-05-20 09:03:25 +0000

    footer modified + minor fixes + some animations

--- js/responsive.js
+++ js/responsive.js
@@ -1,8 +1,24 @@
-const myFunction = function () {
-  const x = document.getElementById("navbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
+const toggleResponsiveNavbar = function () {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("responsive");
+  // if (navbar.className === "navbar") {
+  //   navbar.className += " responsive";
+  // } else {
+  //   navbar.className = "navbar";
+  // }
+};
+
+const activate = function () {
+  console.log("executed");
+  const elements = document.getElementById("navbar").getElementsByTagName("a");
+  for (const a of elements) {
+    let href = a.getAttribute("href");
+    if (href === null) continue;
+    if (href.startsWith(".")) href = href.substring(1);
+
+    if (href == location.pathname) {
+      a.classList.add("selected");
+      return;
     }
-}
+  }
+};
