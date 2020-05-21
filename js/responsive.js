d5d4f5271c8a9ae8a00a31e1fc54a3b85e066c6f
yousm <y.mekkaoui.598@gmail.com>
2020-05-21 21:20:04 +0000

    using fetch function to navigate between tabs

--- js/responsive.js
+++ js/responsive.js
@@ -8,17 +8,42 @@ const toggleResponsiveNavbar = function () {
   // }
 };
 
-const activate = function () {
-  console.log("executed");
-  const elements = document.getElementById("navbar").getElementsByTagName("a");
-  for (const a of elements) {
-    let href = a.getAttribute("href");
-    if (href === null) continue;
-    if (href.startsWith(".")) href = href.substring(1);
+// const activate = function () {
+//   console.log("executed");
+//   const elements = document.getElementById("navbar").getElementsByTagName("a");
+//   for (const a of elements) {
+//     let href = a.getAttribute("href");
+//     if (href === null) continue;
+//     if (href.startsWith(".")) href = href.substring(1);
 
-    if (href == location.pathname) {
-      a.classList.add("selected");
-      return;
+//     if (href == location.pathname) {
+//       a.classList.add("selected");
+//       return;
+//     }
+// };
+
+const loadContent = function( e ) {
+  const id = e.target.id;
+  console.log(e.target);
+  fetch('./' + id + '.html')
+.then(function (data) {
+  return data.text();
+})
+.then(function (html) {
+  document.getElementById('main').innerHTML = html;
+  var scripts = document.getElementById("main").querySelectorAll("script");
+  for (var i = 0; i < scripts.length; i++) {
+    if (scripts[i].innerText) {
+      eval(scripts[i].innerText);
+    } else {
+      fetch(scripts[i].src).then(function (data) {
+        data.text().then(function (r) {
+          eval(r);
+        })
+      });
+
     }
+    scripts[i].parentNode.removeChild(scripts[i]);
   }
+});
 };
