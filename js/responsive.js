a13d2bc26aa61354443b2bb4699b31d1e0540503
Hamza Mekaoui <mekaoui.dev@gmail.com>
2020-05-26 00:01:44 +0100

    fix home page style

--- js/responsive.js
+++ js/responsive.js
@@ -47,3 +47,29 @@ const loadContent = function( e ) {
   }
 });
 };
+
+const displayProgram = function (e) {
+  const id = e.target.id;
+  let tableId;
+  switch (id) {
+    case "semestrHehead1":
+      tableId = "table1";
+      break;
+    case "semestrHehead2":
+      tableId = "table2";
+    break;
+    case "semestrHehead3":
+      tableId = "table3";
+    break;
+    case "semestrHehead4":
+      tableId = "table4";
+    break;
+  }
+  const table = document.getElementById(tableId);
+  if (window.getComputedStyle(table).display === 'table') {
+		table.style["display"] = "none";
+  }
+  else {
+	table.style["display"] = "table";
+  }
+}
