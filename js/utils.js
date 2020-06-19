b2a0358a47c37236ec9dbac2042259f4a2e79d4e
Yousra Mekkaoui <yousra.mekkaoui@lunorsys.de>
2020-06-19 17:04:34 +0100

    progress bar added

--- js/utils.js
+++ js/utils.js
@@ -65,3 +65,10 @@ const scrollTo = (element) => {
 //   element.color = element.backgroundColor;
 //   element.backgroundColor = element.color;
 // };
+
+const progressBar = function() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+};
